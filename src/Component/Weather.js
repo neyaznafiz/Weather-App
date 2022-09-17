c3ad21facb9783d0f01a9a22ecc8bc49f31700e4
neyaznafiz <niiyazobug@gmail.com>
2022-09-18 05:07:53 +0600

    responsive done

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -32,11 +32,11 @@ function Weather() {
                 <h1 className='text-white text-4xl text-center uppercase'>Search your city name </h1>
                 <form onSubmit={searchTemperature} className="md-6 m-auto py-5">
 
-                    <div className="flex gap-x-4 relative mb-3">
-                        <input type="text" name='city' className="h-[48px] w-[400px] rounded-full px-5 bg-black text-white outline-none border border-white" placeholder="Enter your location ..." ></input>
+                    <div className="flex gap-x-4 relative mb-3 mx-5 md:mx-0 lg:mx-0">
+                        <input type="text" name='city' className="h-[48px] w-[80%] lg:w-[400px] md:w-[83%] rounded-full px-5 bg-black text-white outline-none border border-white" placeholder="Enter your location ..." ></input>
 
                         <div className="">
-                            <input type="submit" value="Search" className="bg-white px-8 py-3 rounded-full absolute right-0" />
+                            <input type="submit" value="Search" className="bg-white px-8 py-3 rounded-full absolute left- right-0 md:right-0 lg:right-0" />
                         </div>
                     </div>
 
@@ -44,7 +44,7 @@ function Weather() {
             </div>
 
             {/* data show section */}
-            <div className="grid justify-center text-white  -mt-72">
+            <div className="grid justify-center text-white -mt-24 lg:-mt-72 md:-mt-52">
 
                 <div className='grid justify-center'>
 
