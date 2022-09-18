1ba45731ca87e8cf8bc80c06551b5f95f0eeb44c
neyaznafiz <niiyazobug@gmail.com>
2022-09-18 15:39:56 +0600

    loading added

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -9,12 +9,14 @@ function Weather() {
     // state for store data from fetch
     const [weatherData, setWeatherData] = useState({})
     const { name, main, weather, visibility, wind, sys } = weatherData
+    const [loading, setLoading] = useState(false)
 
     const condition = weather?.[0]?.main
 
     // search by city function
     const searchTemperature = (event) => {
         event.preventDefault();
+        setLoading(true)
 
         const city = event.target.city.value;
 
@@ -22,6 +24,7 @@ function Weather() {
             .then(res => {
                 const { data } = res
                 setWeatherData(data)
+                setLoading(false)
             })
     }
 
@@ -44,88 +47,94 @@ function Weather() {
             </div>
 
             {/* data show section */}
-            <div className="grid justify-center text-white -mt-24 lg:-mt-72 md:-mt-52">
+            <div className='h-full'>
+                {loading ?
+                    <div className='mx-auto border-4 border-white border-t-0 animate-spin h-32 w-32 rounded-full' />
+                    :
+                    <div className="grid justify-center text-white -mt-6 lg:-mt-20 md:-mt-20">
 
-                <div className='grid justify-center'>
+                        <div className='grid justify-center'>
 
-                    <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" className='mx-auto' />
+                            <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="" className='mx-auto' />
 
-                    {condition ?
-                        <h1 className="text-xl text-center -mt-4">{condition}</h1>
-                        :
-                        <h1 className="text-xl text-center -mt-4">Condition</h1>
-                    }
-
-                    {name ?
-                        <h1 className='text-5xl'>{name}</h1>
-                        :
-                        <h1 className='text-5xl'>Location</h1>
-                    }
-                </div>
-
-                {/* temp info */}
-                <div>
-                    <div className='flex justify-around w-96 border-b border-white my-4'>
-                        <span>
-                            {main?.temp ?
-                                <h3 className='text-2xl text-center my-4'><span>{main?.temp}</span>&deg;c</h3>
+                            {condition ?
+                                <h1 className="text-xl text-center -mt-4">{condition}</h1>
                                 :
-                                <h3 className='text-2xl text-center my-2'><span>Temp</span>&deg;c</h3>
+                                <h1 className="text-xl text-center -mt-4">Condition</h1>
                             }
-                        </span>
 
-                        <span>
-                            {visibility ?
-                                <h3 className='text-2xl text-center my-4'><span>{visibility}v</span></h3>
+                            {name ?
+                                <h1 className='text-5xl'>{name}</h1>
                                 :
-                                <h3 className='text-2xl text-center my-2'><span>Visibility</span></h3>
+                                <h1 className='text-5xl'>Location</h1>
                             }
-                        </span>
+                        </div>
+
+                        {/* temp info */}
+                        <div>
+                            <div className='flex justify-around w-96 border-b border-white my-4'>
+                                <span>
+                                    {main?.temp ?
+                                        <h3 className='text-2xl text-center my-4'><span>{main?.temp}</span>&deg;c</h3>
+                                        :
+                                        <h3 className='text-2xl text-center my-2'><span>Temp</span>&deg;c</h3>
+                                    }
+                                </span>
+
+                                <span>
+                                    {visibility ?
+                                        <h3 className='text-2xl text-center my-4'><span>{visibility}v</span></h3>
+                                        :
+                                        <h3 className='text-2xl text-center my-2'><span>Visibility</span></h3>
+                                    }
+                                </span>
+                            </div>
+
+                            {/* wind info */}
+                            <p className='text-center text-xl mt-4'>Wind Info</p>
+                            <div className='flex justify-around w-96 border-b border-white mb-4'>
+                                <span>
+                                    {wind ?
+                                        <h3 className='text-2xl text-center my-4'>Speed: <span>{wind?.speed}</span></h3>
+                                        :
+                                        <h3 className='text-2xl text-center my-2'><span>Wind Speed</span></h3>
+                                    }
+                                </span>
+
+                                <span>
+                                    {wind ?
+                                        <h3 className='text-2xl text-center my-4'>Deg: <span>{wind?.deg}</span>&deg;</h3>
+                                        :
+                                        <h3 className='text-2xl text-center my-2'><span>Wind deg</span></h3>
+                                    }
+                                </span>
+                            </div>
+
+                            {/* sunrise and sunset info */}
+                            <p className='text-center text-xl mt-4'>Country: {sys?.country}</p>
+                            <div className='flex justify-around w-96 border-b border-white mb-4'>
+                                <span>
+                                    <p className='text-center text-xl'>Sunrise</p>
+                                    {sys ?
+                                        <h3 className='text-2xl text-center my-4'><span>{sys?.sunrise}</span></h3>
+                                        :
+                                        <h3 className='text-2xl text-center my-2'><span>Time</span></h3>
+                                    }
+                                </span>
+
+                                <span>
+                                    <p className='text-center text-xl'>Sunset</p>
+                                    {sys ?
+                                        <h3 className='text-2xl text-center my-4'><span>{sys?.sunset}</span></h3>
+                                        :
+                                        <h3 className='text-2xl text-center my-2'><span>Time</span></h3>
+                                    }
+                                </span>
+                            </div>
+
+                        </div>
                     </div>
-
-                    {/* wind info */}
-                    <p className='text-center text-xl mt-4'>Wind Info</p>
-                    <div className='flex justify-around w-96 border-b border-white mb-4'>
-                        <span>
-                            {wind ?
-                                <h3 className='text-2xl text-center my-4'>Speed: <span>{wind?.speed}</span></h3>
-                                :
-                                <h3 className='text-2xl text-center my-2'><span>Wind Speed</span></h3>
-                            }
-                        </span>
-
-                        <span>
-                            {wind ?
-                                <h3 className='text-2xl text-center my-4'>Deg: <span>{wind?.deg}</span>&deg;</h3>
-                                :
-                                <h3 className='text-2xl text-center my-2'><span>Wind deg</span></h3>
-                            }
-                        </span>
-                    </div>
-
-                    {/* sunrise and sunset info */}
-                    <p className='text-center text-xl mt-4'>Country: {sys?.country}</p>
-                    <div className='flex justify-around w-96 border-b border-white mb-4'>
-                        <span>
-                            <p className='text-center text-xl'>Sunrise</p>
-                            {sys ?
-                                <h3 className='text-2xl text-center my-4'><span>{sys?.sunrise}</span></h3>
-                                :
-                                <h3 className='text-2xl text-center my-2'><span>Time</span></h3>
-                            }
-                        </span>
-
-                        <span>
-                            <p className='text-center text-xl'>Sunset</p>
-                            {sys ?
-                                <h3 className='text-2xl text-center my-4'><span>{sys?.sunset}</span></h3>
-                                :
-                                <h3 className='text-2xl text-center my-2'><span>Time</span></h3>
-                            }
-                        </span>
-                    </div>
-
-                </div>
+                }
             </div>
         </div>
     )
